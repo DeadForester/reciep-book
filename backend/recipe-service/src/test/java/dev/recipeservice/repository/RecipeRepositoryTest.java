bd1753244f3b84de772310621e9b5a87179c26cb
Mathew <mattew1997@yandex.ru>
2026-04-12 16:48:47 +0300

    refactor: fix сi.yaml file.

--- backend/recipe-service/src/test/java/dev/recipeservice/repository/RecipeRepositoryTest.java
+++ backend/recipe-service/src/test/java/dev/recipeservice/repository/RecipeRepositoryTest.java
@@ -5,11 +5,25 @@ import dev.recipeservice.repository.recipe.RecipeRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataMongoTest
+@Testcontainers
 class RecipeRepositoryTest {
+
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
 
     @Autowired
     private RecipeRepository repository;
